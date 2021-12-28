86dd069dafdcbfb109d892df50532ad58c600ca8
Caiobkw <47906862+Caiobkw@users.noreply.github.com>
2021-12-28 17:22:53 -0300

    +

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -5,28 +5,10 @@ import useAsyncEffect from "use-async-effect";
 const DropdownMenu: React.FC<{active: boolean, width: number}> = (props) => {
 
     let dropdownRef: HTMLDivElement | null = null;
-    let isActive = false;
-    let realActive = false;
-
-    const handleLeave = async () => {
-        if(!props.active) {
-            await new Promise((r) => {setTimeout(r, 300)});
-            if(isActive) {
-                realActive = true;
-            } else {
-                realActive = false;
-            }
-        } else {realActive = true};
-    };
-
-    
-    useAsyncEffect(async () => {
-        await handleLeave();
-    }, [props.active, isActive]);
 
     return (
         <div className={`${styles.dropdown} ${props.active ? styles.active : styles.unactive}`} style={{width: props.width}} 
-        ref={node => {dropdownRef = node}} onMouseEnter={()=>{ isActive = true;}} onMouseLeave={()=>{ isActive = false;}} > 
+        ref={node => {dropdownRef = node}} > 
             <ul>
                 <li>Dashboard</li>  
                 <SignOut/>
